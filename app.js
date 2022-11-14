252a14aa378849905efb700819af40b08c4ca759
jpsong <jpsong@tencent.com>
2022-11-14 20:42:07 +0800

    feat: first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const port = 3001
+
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const options = {
+  target: 'https://api.github.com', // target host with the same base path
+  pathRewrite: function (path, req) { return path.replace('/proxy', '') },
+  changeOrigin: true // needed for virtual hosted sites
+}
+
+// create the proxy
+const proxy = createProxyMiddleware(options)
+
+// mount `proxy` in web server
+app.use('/proxy', proxy)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://127.0.0.1:${port}`)
+})
